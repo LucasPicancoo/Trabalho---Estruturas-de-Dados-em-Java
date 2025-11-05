d6f992c6096bdf7ba32d4eaf60f9aad8be395fb7
Lucas Picanco <lucasferreira3254@outlook.com>
2025-11-05 10:43:43 -0300

    Criação dos construtores e getters na classe de vetores.

--- Estruturas de Dados em Java/Vetor/Vetor.java
+++ Estruturas de Dados em Java/Vetor/Vetor.java
@@ -1,4 +1,28 @@
 public class Vetor{
    
+   private int[] valores;
+   private int tamanho;
+   
+   //Construtor padrão com tamanho 0
+   public Vetor(int capacidade){
+      this.valores = new int[capacidade];
+      this.tamanho = 0;
+   }
+   
+   //Construtor com o tamanho atual do vetor
+   public Vetor(int[] valores, int tamanho){
+      this.valores = valores;
+      this.tamanho = tamanho;
+   }
+   
+   public int[] getValores(){
+      return valores;
+   }
+   
+   
+   public int getTamanho(){
+      return tamanho;
+   }
+   
    
 }
