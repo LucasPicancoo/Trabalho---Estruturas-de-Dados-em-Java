ec2794f7b46d7d9571913333aabcdffae7adf61b
Lucas Picanco <lucasferreira3254@outlook.com>
2025-11-07 13:02:35 -0300

    Feat: Criação de dois novos geradores de números (gerar1000 e gerar10000)

--- Estruturas de Dados em Java/Utils/Gerador.java
+++ Estruturas de Dados em Java/Utils/Gerador.java
@@ -7,7 +7,23 @@ public class Gerador {
     public void gerar100(Vetor v){
         Random r = new Random();
         for(int i = 1; i <= 100; i++){
+            int numero = r.nextInt(100);
+            v.inserir(numero);
+        }
+    }
+
+    public void gerar1000(Vetor v){
+        Random r = new Random();
+        for(int i = 1; i <= 1000; i++){
             int numero = r.nextInt(1000);
+            v.inserir(numero);
+        }
+    }
+
+    public void gerar10000(Vetor v){
+        Random r = new Random();
+        for(int i = 1; i <= 10000; i++){
+            int numero = r.nextInt(10000);
             v.inserir(numero);
         }
     }
